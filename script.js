220399b9761ed04ac671b0c88eebe5d30fc394ab
Clémence <98712982+clemenceprv@users.noreply.github.com>
2022-03-20 09:12:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // TYPED
 var typed = new Typed('.typed', {
-  strings: ["Bonjour ! Actuellement en première année de cycle d'ingénieur informatique à Polytech Lyon, je cherche l'entreprise avec laquelle je pourrai réaliser une alternance de deux ans. Je suis motivée pour apprendre de nouvelles compétences et approfondir mes connaissances. Au terme de ces deux, j'aimerais profiter de l'expérience accumulée pour poursuivre dans cette même entreprise. En outre, je suis ouverte aux déplacements si cela et nécessaire. Enfin, si cela est possible j'aimerais commencer à travailler dès le mois de juillet en contrat d'intérim pour l'été afin de suivre une formation préalable aux méthodes et outils de l'entreprise."],
+  strings: ["Bonjour ! Actuellement en première année de cycle d'ingénieur informatique à Polytech Lyon, je cherche l'entreprise avec laquelle je pourrai réaliser une alternance de deux ans en contrat d'apprentissage. Je suis motivée pour apprendre de nouvelles compétences et approfondir mes connaissances. Au terme de ces deux, j'aimerais profiter de l'expérience accumulée pour poursuivre dans cette même entreprise. En outre, je suis ouverte aux déplacements si cela et nécessaire. Enfin, si cela est possible j'aimerais commencer à travailler dès le mois de juillet en contrat d'intérim pour l'été afin de suivre une formation préalable aux méthodes et outils de l'entreprise."],
   typeSpeed: 20,
 });
   
