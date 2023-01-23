0fe7ff5d984f3976cff34a6c832dd7fb6ec009b0
Clémence <98712982+clemenceprv@users.noreply.github.com>
2023-01-23 08:22:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // TYPED
 var typed = new Typed('.typed', {
-  strings: ["Bonjour ! Actuellement en première année de cycle d'ingénieur informatique à Polytech Lyon, je cherche l'entreprise avec laquelle je pourrai réaliser un stage de 20 semaines. Je suis motivée pour approfondir mes connaissances et apprendre de nouvelles compétences. Au terme de ce stage, j'aimerais profiter de l'expérience accumulée pour poursuivre en alternance en cinquième année dans cette même entreprise. En outre, je suis ouverte aux déplacements. Enfin, si cela est possible j'aimerais commencer à travailler dès le mois de juillet en contrat d'intérim pour l'été afin de suivre une formation préalable aux méthodes et outils de l'entreprise."],
-  typeSpeed: 10,
+  strings: ["Bonjour ! Actuellement en quatrième année en école d'ingénieur informatique, je souhaite réaliser mon alternance dans le secteur de la data pour ma dernière année à Polytech Lyon avec EDF. Je suis motivée pour approfondir mes connaissances et apprendre de nouvelles compétences. Au terme de cette alternance, j'aimerais profiter de l'expérience accumulée pour poursuivre avec vous en CDI. Je cherche de préférence à Nantes ou Lyon, mais je suis ouverte aux déplacements dans toute la France car je souhaite uniquement travailler pour EDF."],
+  typeSpeed: 15,
 });
   
 //AOS
