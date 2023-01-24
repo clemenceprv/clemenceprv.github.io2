d67c30c252bf89d9cc4cb9f5dd17b5f76b8df6f8
Clémence <98712982+clemenceprv@users.noreply.github.com>
2023-01-24 08:46:05 +0100

    delete script

--- script.js
+++
@@ -1,8 +0,0 @@
-// TYPED
-var typed = new Typed('.typed', {
-  strings: ["Bonjour ! Actuellement en quatrième année en école d'ingénieur informatique, je souhaite réaliser mon alternance dans le secteur de la data pour ma dernière année à Polytech Lyon avec EDF. Je suis motivée pour approfondir mes connaissances et apprendre de nouvelles compétences. Au terme de cette alternance, j'aimerais profiter de l'expérience accumulée pour poursuivre avec vous en CDI. Je cherche de préférence à Nantes ou Lyon, mais je suis ouverte aux déplacements dans toute la France car je souhaite uniquement travailler pour EDF."],
-  typeSpeed: 15,
-});
-  
-//AOS
-AOS.init();
